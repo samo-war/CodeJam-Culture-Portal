f6b48c320955845108879bc20dbc6eb41a380018
Yuliya Hlukhatarenka <julkin@gmail.com>
2019-01-26 23:20:48 +0300

    feat: implement search-page

---
+++ search-page/index.js
@@ -0,0 +1,53 @@
+const searcher = document.getElementById('search-input');
+const resultsSection = document.getElementById('results-section');
+let arr;
+
+window.onload = loadJson('');
+searcher.addEventListener("keyup", onClickEnterInSearch);
+
+function onClickEnterInSearch(e) {
+    const str = searcher.value;
+    if (e.keyCode === 13) {
+        loadResults(str);
+    }
+}
+
+function loadResults(str) {
+    resultsSection.innerHTML = '';
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i].name.includes(str) || arr[i].place.includes(str) || str === ''){
+            const photographSection = document.createElement('div');
+            photographSection.className = 'photograph-section';
+
+            const link = document.createElement('a');
+            link.href = arr[i].link;
+
+            const img = document.createElement('img');
+            img.setAttribute('src',`./assets/images/${arr[i].pict}`);
+            img.setAttribute('alt', 'photographer photo');
+            img.className = 'image';
+            link.appendChild(img);
+
+            const textContainer = document.createElement('div');
+            const text = document.createElement('p');
+            text.innerText = arr[i].info;
+            textContainer.appendChild(text);
+
+            photographSection.appendChild(link);
+            photographSection.appendChild(textContainer);
+            resultsSection.appendChild(photographSection);
+        }
+    
+    }
+}
+
+function loadJson(str) {
+    fetch('./assets/data/data.json')
+      .then(response => response.json())
+      .then((myJson) => {
+        arr = myJson.photographers;
+      })
+      .then(() => loadResults(str))
+      .catch('Can`t load users!');
+   
+  }
